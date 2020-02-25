3161b1e0cd6c005a101db33082bbd9399d25dc88
Sarah Vivoli <snvivoli@gmail.com>
2020-02-25 11:32:28 -0800

    app.js complete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
+const path = require('path'); 
 const nodemailer = require('nodemailer');
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 // View engine setup
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handelbars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
