19263004d23b63bf4a368ad9960a4405b296257c
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 12:18:46 -0700

    portfolio container component

---
+++ portfolio/src/components/PortfolioContainer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Projects from '../Projects';
+import ProjectsLinks from '../ProjectsLinks';
+import '../style.css';
+
+function PortfolioContainer() {
+    return (
+        <div>
+            <Projects />
+            <ProjectsLinks />
+        </div>
+    )
+};
+
+export default PortfolioContainer;
