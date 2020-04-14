3b6de0b38a2e6fdd53b3969620243db337f7e5ab
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 11:21:42 -0700

    footer component

---
+++ portfolio/src/components/Footer/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style.css";
+
+function Footer() {
+    return (
+        <div class="holy-grail-footer">
+            Copyright 2020©
+        </div>
+    )
+};
+
+export default Footer;
