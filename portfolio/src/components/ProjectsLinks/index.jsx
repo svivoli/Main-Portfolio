c873f7fd9ae1dfa671240bb9b6ff935d5d946cfb
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 12:16:08 -0700

    projectslinks component

---
+++ portfolio/src/components/ProjectsLinks/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../style.css';
+
+function ProjectsLinks() {
+    return (
+         <div class="repositories">
+
+                <h3 class="title">Repositories</h3>
+
+                <div class="github-links">   
+                <a href="https://github.com/svivoli/Google-Books-Search" target="_blank"><i class="social-icon fab fa-github"></i></a> Google Books
+                <br></br>
+                <a href="https://github.com/svivoli/Employee-Directory" target="_blank"><i class="social-icon fab fa-github"></i></a> Employee Directory
+                <br></br>
+                <a href="https://github.com/svivoli/Budget-Tracker" target="_blank"><i class="social-icon fab fa-github"></i></a> Budget Tracker
+                <br></br>
+                <a href="https://github.com/svivoli/Workout-Tracker" target="_blank"><i class="social-icon fab fa-github"></i></a> Workout Tracker
+                <br></br>
+                <a href="https://github.com/Jeretc79/project1" target="_blank"><i class="social-icon fab fa-github"></i></a> ShowMeShows
+                <br></br>
+                <a href="https://github.com/marissa-lc/Peer-One" target="_blank"><i class="social-icon fab fa-github"></i></a> peɘr up
+                <br></br>
+                <a href="https://github.com/svivoli/Weather-Dashboard" target="_blank"><i class="social-icon fab fa-github"></i></a> Weather Dash
+                <br></br>
+                <a href="https://github.com/svivoli/Daily-Planner" target="_blank"><i class="social-icon fab fa-github"></i></a> Daily Planner
+                <br></br>
+                <a href="https://github.com/svivoli/Burger-Logger" target="_blank"><i class="social-icon fab fa-github"></i></a> Burger Logger
+                <br></br>
+                <a href="https://github.com/svivoli/Note-Taker" target="_blank"><i class="social-icon fab fa-github"></i></a> Note Taker
+                <br></br>
+                <a href="https://github.com/svivoli/portfolio1" target="_blank"><i class="social-icon fab fa-github"></i></a> Early Portfolio
+                <br></br>
+                <a href="https://svivoli.github.io/portfolio-Pink-Grey/" target="_blank"><i class="social-icon fab fa-github"></i></a> Later Portfolio 1
+                <br></br>
+                <a href="https://svivoli.github.io/portfolio-Simple-Blue/" target="_blank"><i class="social-icon fab fa-github"></i></a> Later Portfolio 2
+                <br></br><br></br>
+
+                <a href="https://github.com/svivoli?tab=repositories" target="_blank"><i class="social-icon fab fa-github"></i></a> All GitHub Repositories
+            </div>
+        </div>
+    )
+};
+
+export default ProjectsLinks;
