d5af2e895caef83069d2a206806b224f6bd9d16b
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 12:12:34 -0700

    projects component

---
+++ portfolio/src/components/Projects/index.jsx
@@ -0,0 +1,189 @@
+import React from 'react';
+import '../style.css';
+
+function Projects() {
+    return (
+        <article class="article-portfolio">
+
+            <section class="portfolio1">
+                <div class="image-hover-wrapper">
+                    <a href="https://g00gleb00ks.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/books.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+
+                            <p><strong>Google Books</strong><br></br>Search for and save books from Google<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio1">
+                <div class="image-hover-wrapper">
+                    <a href="https://employee-direct0ry.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/employee.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+
+                            <p><strong>Employee Directory</strong><br></br>Search and sort employees<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio1">
+                <div class="image-hover-wrapper">
+                    <a href="https://track-your-budg3t.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/budget.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+
+                            <p><strong>Budget Tracker</strong><br></br>Track your spending and savings<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio1">
+                <div class="image-hover-wrapper">
+                    <a href="https://w0rkout-tracker.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/fitness.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+
+                            <p><strong>Workout Tracker</strong><br></br>Track your workout progress<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio1">
+                <div class="image-hover-wrapper">
+                    <a href="https://jeretc79.github.io/project1/" target="_blank"><img src="assets/images/portfolio/showmeshows.JPEG"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+
+                            <p><strong>SHOWMESHOWS</strong><br></br>Concert Locator and Reminder<br></br>(Currently under maintenance)<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio3">
+                <div class="image-hover-wrapper">
+                    <a href="https://peer-one.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/peer-up.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>peɘr up</strong><br></br>A collaborative website for coding students<br></br><br></br>
+                        Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio2">
+                <div class="image-hover-wrapper">
+                    <a href="https://svivoli.github.io/Weather-Dashboard/" target="_blank"><img src="assets/images/portfolio/weatherapp.jpg"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>WEATHER DASH</strong><br></br>Displays current and forecasted weather based on searched
+                            criteria<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio3">
+                <div class="image-hover-wrapper">
+                    <a href="https://svivoli.github.io/Daily-Planner/" target="_blank"><img src="assets/images/portfolio/planner.jpg"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>DAILY PLANNER</strong><br></br>Organize your daily tasks by hour using local
+                            storage<br></br><br></br>
+                            Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio3">
+                <div class="image-hover-wrapper">
+                    <a href="https://stark-everglades-05320.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/diner.jpg"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>Burger Logger</strong><br></br>A log for burger lovers<br></br><br></br>
+                        Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio3">
+                <div class="image-hover-wrapper">
+                    <a href="https://aqueous-journey-39158.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/note-taker.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>Note Taker</strong><br></br>Write, save and delete notes<br></br><br></br>
+                        Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio4">
+                <div class="image-hover-wrapper">
+                    <a href="https://svivoli.github.io/Portfolio1/" target="_blank"><img src="assets/images/portfolio/portfolio.png"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>Early Portfolio</strong><br></br><br></br>
+                        Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio3">
+                <div class="image-hover-wrapper">
+                    <a href="https://svivoli.github.io/Portfolio-Pink-Grey/" target="_blank"><img src="assets/images/portfolio/beachfull1.jpg"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>Later Portfolio 1</strong><br></br>Featuring continuous scroll and generated theme<br></br><br></br>
+                        Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+            <section class="portfolio3">
+                <div class="image-hover-wrapper">
+                    <a href="https://svivoli.github.io/Portfolio-Simple-Blue/" target="_blank"><img src="assets/images/portfolio/desk1.jpg"
+                        width="600"></img>
+                        <span class="image-hover-wrapper-reveal">
+                            <p><strong>Later Portfolio 2</strong><br></br>Featuring continuous scroll and another generated theme<br></br><br></br>
+                        Check it<br></br><i class="fa fa-link" aria-hidden="true"></i>
+                            </p>
+                        </span>
+                    </a>
+                </div>
+            </section>
+
+
+        </article>
+    )
+};
+
+export default Projects;
