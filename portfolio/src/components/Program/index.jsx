e38cffb27e978107ed365b96b0a1b0580e3f64b0
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 12:01:12 -0700

    program component

---
+++ portfolio/src/components/Program/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style.css';
+
+function Program() {
+    return (
+        <section class="bootcamp-section"
+            class="uk-height-large uk-background-cover uk-light uk-flex-vertical-alignment" uk-parallax="bgy: -200"
+            style="background-image: url(https://i.imgur.com/dD5Zk5E.png);">
+
+            <br></br>
+            <h2 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical"><a class="bootcamp-link"
+                    href="https://bootcamp.uoregon.edu/coding/" target="_blank">
+                    Learn more about the University of Oregon's Boot Camp program here.
+                </a></h2>
+
+        </section>
+    )
+};
+
+export default Program;
