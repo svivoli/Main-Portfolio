b1dcfa44bd2925cb664f77e8dc6c197b5c935e8f
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 11:20:33 -0700

    header added to home page

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Header from "../components/Header";
 import "./style.css";
 
 function Home() {
     return (
-
+        <div>
+            <Header />
+        </div>
     )
 }
 
