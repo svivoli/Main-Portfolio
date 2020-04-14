20046511e10e83154ccc0f2850a6f870bad75f35
Sarah Vivoli <snvivoli@gmail.com>
2020-04-14 11:22:18 -0700

    footer added to home page

--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import "./style.css";
 
 function Home() {
     return (
         <div>
             <Header />
+            <Footer />
         </div>
     )
 }
