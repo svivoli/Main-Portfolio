33cebd3b5ce6aa130f1aabbff6b7c79f45e13558
Sarah Vivoli <snvivoli@gmail.com>
2020-04-21 12:07:50 -0700

    inline style for bootcamp-section background removed

--- client/src/components/Program/index.jsx
+++ client/src/components/Program/index.jsx
@@ -4,8 +4,7 @@ import '../style.css';
 function Program() {
     return (
         <section class="bootcamp-section"
-            class="uk-height-large uk-background-cover uk-light uk-flex-vertical-alignment" uk-parallax="bgy: -200"
-            style="background-image: url(https://i.imgur.com/dD5Zk5E.png);">
+            class="uk-height-large uk-background-cover uk-light uk-flex-vertical-alignment" uk-parallax="bgy: -200">
 
             <br></br>
             <h2 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical"><a class="bootcamp-link"
