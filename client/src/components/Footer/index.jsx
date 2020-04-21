182dad971e523b174f8eeede677fa6cfcb55a0b1
Sarah Vivoli <snvivoli@gmail.com>
2020-04-21 12:09:03 -0700

    social icons moved to footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,8 +4,22 @@ import '../style.css';
 function Footer() {
     return (
         <div class="holy-grail-footer">
-            Copyright 2020©
+
+        <div class="social-btns">
+            <ul>
+                <li><a href="mailto:snvivoli@gmail.com" class="social-btn"><i class="social-icon fas fa-paper-plane"></i></a></li>
+
+                <li><a href="https://github.com/svivoli" class="social-btn" target="_blank"><i
+                            class="social-icon fab fa-github"></i></a></li>
+
+                <li><a href="https://www.linkedin.com/in/sarah-vivoli/" class="social-btn" target="_blank"><i
+                            class="social-icon fab fa-linkedin"></i></a></li>
+
+                <li><a href="https://docs.google.com/document/d/1Dgleh29HBpD2juFqEO7xUXNbs7VSOeoedjUGzAnZqHc/edit?usp=sharing"
+                        class="social-btn" target="_blank"><i class="social-icon fab fa-google-drive"></i></a></li>
+            </ul>
         </div>
+    </div>
     )
 };
 
