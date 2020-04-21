90c47d702c833d83009c4dc63d4947729032e8ae
Sarah Vivoli <snvivoli@gmail.com>
2020-04-21 12:43:14 -0700

    portfolio project image references

--- client/src/components/Projects/index.jsx
+++ client/src/components/Projects/index.jsx
@@ -7,8 +7,8 @@ function Projects() {
 
             <section class="portfolio1">
                 <div class="image-hover-wrapper">
-                    <a href="https://u-incahoots.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/cahoots.png"
-                            width="600"></img>
+                    <a href="https://u-incahoots.herokuapp.com/" target="_blank"><img src="../../images/portfolio/cahoots.png"
+                        style={{width: "600px"}}></img>
                         <span class="image-hover-wrapper-reveal">
 
                             <p><strong>Cahoots</strong><br></br>Curate and join events in your neighborhood<br></br><br></br>
@@ -21,8 +21,8 @@ function Projects() {
 
             <section className="portfolio1">
                 <div className="image-hover-wrapper">
-                    <a href="https://g00gleb00ks.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/books.png"
-                        width="600"></img>
+                    <a href="https://g00gleb00ks.herokuapp.com/" target="_blank"><img src="../../images/portfolio/books.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
 
                             <p><strong>Google Books</strong><br></br>Search for and save books from Google<br></br><br></br>
@@ -35,8 +35,8 @@ function Projects() {
 
             <section className="portfolio1">
                 <div className="image-hover-wrapper">
-                    <a href="https://employee-direct0ry.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/employee.png"
-                        width="600"></img>
+                    <a href="https://employee-direct0ry.herokuapp.com/" target="_blank"><img src="../../images/portfolio/employee.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
 
                             <p><strong>Employee Directory</strong><br></br>Search and sort employees<br></br><br></br>
@@ -49,8 +49,8 @@ function Projects() {
 
             <section className="portfolio1">
                 <div className="image-hover-wrapper">
-                    <a href="https://track-your-budg3t.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/budget.png"
-                        width="600"></img>
+                    <a href="https://track-your-budg3t.herokuapp.com/" target="_blank"><img src="../../images/portfolio/budget.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
 
                             <p><strong>Budget Tracker</strong><br></br>Track your spending and savings<br></br><br></br>
@@ -63,8 +63,8 @@ function Projects() {
 
             <section className="portfolio1">
                 <div className="image-hover-wrapper">
-                    <a href="https://w0rkout-tracker.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/fitness.png"
-                        width="600"></img>
+                    <a href="https://w0rkout-tracker.herokuapp.com/" target="_blank"><img src="../../images/portfolio/fitness.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
 
                             <p><strong>Workout Tracker</strong><br></br>Track your workout progress<br></br><br></br>
@@ -77,8 +77,8 @@ function Projects() {
 
             <section className="portfolio1">
                 <div className="image-hover-wrapper">
-                    <a href="https://jeretc79.github.io/project1/" target="_blank"><img src="assets/images/portfolio/shows.png"
-                        width="600"></img>
+                    <a href="https://jeretc79.github.io/project1/" target="_blank"><img src="../../images/portfolio/shows.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
 
                             <p><strong>SHOWMESHOWS</strong><br></br>Concert Locator and Reminder<br></br>(Currently under maintenance)<br></br><br></br>
@@ -91,8 +91,8 @@ function Projects() {
 
             <section className="portfolio3">
                 <div className="image-hover-wrapper">
-                    <a href="https://peer-one.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/peerup.png"
-                        width="600"></img>
+                    <a href="https://peer-one.herokuapp.com/" target="_blank"><img src="../../images/portfolio/peerup.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>peɘr up</strong><br></br>A collaborative website for coding students<br></br><br></br>
                         Check it<br></br><i className="fa fa-link" aria-hidden="true"></i>
@@ -104,8 +104,8 @@ function Projects() {
 
             <section className="portfolio2">
                 <div className="image-hover-wrapper">
-                    <a href="https://svivoli.github.io/Weather-Dashboard/" target="_blank"><img src="assets/images/portfolio/weather.png"
-                        width="600"></img>
+                    <a href="https://svivoli.github.io/Weather-Dashboard/" target="_blank"><img src="../../images/portfolio/weather.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>WEATHER DASH</strong><br></br>Displays current and forecasted weather based on searched
                             criteria<br></br><br></br>
@@ -118,8 +118,8 @@ function Projects() {
 
             <section className="portfolio3">
                 <div className="image-hover-wrapper">
-                    <a href="https://svivoli.github.io/Daily-Planner/" target="_blank"><img src="assets/images/portfolio/daily.png"
-                        width="600"></img>
+                    <a href="https://svivoli.github.io/Daily-Planner/" target="_blank"><img src="../../images/portfolio/daily.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>DAILY PLANNER</strong><br></br>Organize your daily tasks by hour using local
                             storage<br></br><br></br>
@@ -132,8 +132,8 @@ function Projects() {
 
             <section className="portfolio3">
                 <div className="image-hover-wrapper">
-                    <a href="https://stark-everglades-05320.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/burger.png"
-                        width="600"></img>
+                    <a href="https://stark-everglades-05320.herokuapp.com/" target="_blank"><img src="../../images/portfolio/burger.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>Burger Logger</strong><br></br>A log for burger lovers<br></br><br></br>
                         Check it<br></br><i className="fa fa-link" aria-hidden="true"></i>
@@ -145,8 +145,8 @@ function Projects() {
 
             <section className="portfolio3">
                 <div className="image-hover-wrapper">
-                    <a href="https://aqueous-journey-39158.herokuapp.com/" target="_blank"><img src="assets/images/portfolio/note.png"
-                        width="600"></img>
+                    <a href="https://aqueous-journey-39158.herokuapp.com/" target="_blank"><img src="../../images/portfolio/note.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>Note Taker</strong><br></br>Write, save and delete notes<br></br><br></br>
                         Check it<br></br><i className="fa fa-link" aria-hidden="true"></i>
@@ -158,8 +158,8 @@ function Projects() {
 
             <section className="portfolio4">
                 <div className="image-hover-wrapper">
-                    <a href="https://svivoli.github.io/Portfolio1/" target="_blank"><img src="assets/images/portfolio/portfolio0.png"
-                        width="600"></img>
+                    <a href="https://svivoli.github.io/Portfolio1/" target="_blank"><img src="../../images/portfolio/portfolio0.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>Early Portfolio</strong><br></br><br></br>
                         Check it<br></br><i className="fa fa-link" aria-hidden="true"></i>
@@ -171,8 +171,8 @@ function Projects() {
 
             <section className="portfolio3">
                 <div className="image-hover-wrapper">
-                    <a href="https://svivoli.github.io/Portfolio-Pink-Grey/" target="_blank"><img src="assets/images/portfolio/portfolio1.png"
-                        width="600"></img>
+                    <a href="https://svivoli.github.io/Portfolio-Pink-Grey/" target="_blank"><img src="../../images/portfolio/portfolio1.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>Later Portfolio 1</strong><br></br>Featuring continuous scroll and generated theme<br></br><br></br>
                         Check it<br></br><i className="fa fa-link" aria-hidden="true"></i>
@@ -184,8 +184,8 @@ function Projects() {
 
             <section className="portfolio3">
                 <div className="image-hover-wrapper">
-                    <a href="https://svivoli.github.io/Portfolio-Simple-Blue/" target="_blank"><img src="assets/images/portfolio/portfolio2.png"
-                        width="600"></img>
+                    <a href="https://svivoli.github.io/Portfolio-Simple-Blue/" target="_blank"><img src="../../images/portfolio/portfolio2.png"
+                        style={{width: "600px"}}></img>
                         <span className="image-hover-wrapper-reveal">
                             <p><strong>Later Portfolio 2</strong><br></br>Featuring continuous scroll and another generated theme<br></br><br></br>
                         Check it<br></br><i className="fa fa-link" aria-hidden="true"></i>
