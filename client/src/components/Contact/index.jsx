583c6585f31a3847ef8b1e5ceec53d6547225d37
Sarah Vivoli <snvivoli@gmail.com>
2020-04-21 12:08:33 -0700

    contact section grid and blurb

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -5,32 +5,27 @@ function Contact() {
     return (
         <section class="contact-section">
             <h2 class="title">Get in touch</h2>
-            <div class="contact-card">
-                <i class="contact-icon fas fa-paper-plane"></i>
-                <h5>Email</h5>
-                <a href="mailto:snvivoli@gmail.com" class="contact"> snvivoli@gmail.com</a>
+            <div class="contact-grid uk-grid-collapse uk-child-width-expand@s" uk-grid>
+                <div class="uk-width-1-4">
+                    <div class="contact-card">
+                        <i class="contact-icon fas fa-paper-plane"></i>
+                        <h5>Email</h5>
+                        <a href="mailto:snvivoli@gmail.com" class="contact"> snvivoli@gmail.com</a>
+                    </div>
+                </div>
+                <div class="contact-blurb uk-width-1-2">
+                    <p>Comment on my work, connect me with a colleague, or send me a business inquiry. I even accept restaurant suggestions in the Portland area.</p>
+                </div>
+                <div class="uk-width-1-4">
+                    <div class="contact-card">
+                        <i class="contact-icon fas fa-mobile-alt"></i>
+                        <h5>Phone</h5>
+                        <a href="tel:16507962557" class="contact">(650) 796-2557</a>
+                    </div>
+                </div>
             </div>
-            <br></br>
-            <div class="contact-card">
-                <i class="contact-icon fas fa-mobile-alt"></i>
-                <h5>Phone</h5>
-                (650) 796-2557
-            </div>
 
-            <div class="social-btns">
-                <ul>
-                    <li><a href="mailto:snvivoli@gmail.com" class="social-btn"><i class="social-icon fas fa-paper-plane"></i></a></li>
-
-                    <li><a href="https://github.com/svivoli" class="social-btn" target="_blank"><i
-                                class="social-icon fab fa-github"></i></a></li>
-
-                    <li><a href="https://www.linkedin.com/in/sarah-vivoli/" class="social-btn" target="_blank"><i
-                                class="social-icon fab fa-linkedin"></i></a></li>
-
-                    <li><a href="https://docs.google.com/document/d/1Dgleh29HBpD2juFqEO7xUXNbs7VSOeoedjUGzAnZqHc/edit?usp=sharing"
-                            class="social-btn" target="_blank"><i class="social-icon fab fa-google-drive"></i></a></li>
-                </ul>
-            </div>
+            
         </section>
     )
 };
