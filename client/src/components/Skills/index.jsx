8db18d7f159dbad41f7acf59ea79f290d8905241
Sarah Vivoli <snvivoli@gmail.com>
2020-04-21 12:04:22 -0700

    skills changed to buttons

---
+++ client/src/components/Skills/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../style.css';
+
+function Skills() {
+    return (
+        <section class="skills-section" style="background-image: url(https://i.imgur.com/FOiQ19p.png);">
+
+            <h2 class="title">Skills</h2>
+
+            <button class="skill-btn" type="button" disabled>HTML</button>
+            <button class="skill-btn" type="button" disabled>CSS</button>
+            <button class="skill-btn" type="button" disabled>Javascript</button>
+            <button class="skill-btn" type="button" disabled>jQuery</button>
+            <button class="skill-btn" type="button" disabled>Node</button>
+            <button class="skill-btn" type="button" disabled>Api's</button>
+            <button class="skill-btn" type="button" disabled>Express</button>
+            <br></br>
+            <button class="skill-btn" type="button" disabled>MySQL</button>
+            <button class="skill-btn" type="button" disabled>NoSQL</button>
+            <button class="skill-btn" type="button" disabled>PWA</button>
+            <button class="skill-btn" type="button" disabled>MongoDB</button>
+            <button class="skill-btn" type="button" disabled>IndexedDB</button>
+            <button class="skill-btn" type="button" disabled>React</button>
+
+        </section>
+    )
+};
+
+export default Skills;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const router = require("express").Router();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+// Routes
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+
+app.listen(PORT, function () {
+    console.log(`🌎 Server listening on port ${PORT}!`);
+});
