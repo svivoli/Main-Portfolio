6ea0eaa316a4e96b8ab77359910303ecfe76ac41
Sarah Vivoli <snvivoli@gmail.com>
2020-04-22 12:23:23 -0700

    contact section grid

--- client/src/components/Contact/index.jsx
+++ client/src/components/Contact/index.jsx
@@ -5,21 +5,21 @@ function Contact() {
     return (
         <section className="contact-section">
             <h2 className="title">Get in touch</h2>
-            <div className="contact-grid uk-grid-collapse uk-child-width-expand@s" uk-grid>
-                <div className="uk-width-1-4">
+            <div className="row">
+                <div className="contact-grid-item col-lg-3 col-md-3 col-sm-12">
                     <div className="contact-card">
                         <i className="contact-icon fas fa-paper-plane"></i>
-                        <h5>Email</h5>
-                        <a href="mailto:snvivoli@gmail.com" className="contact"> snvivoli@gmail.com</a>
+                        <br></br>
+                        <a href="mailto:snvivoli@gmail.com" className="contact">snvivoli@gmail.com</a>
                     </div>
                 </div>
-                <div className="contact-blurb uk-width-1-2">
+                <div className="contact-grid-item col-lg-6 col-md-6 col-sm-12">
                     <p>Comment on my work, connect me with a colleague, or send me a business inquiry. I even accept restaurant suggestions in the Portland area.</p>
                 </div>
-                <div className="uk-width-1-4">
+                <div className="contact-grid-item col-lg-3 col-md-3 col-sm-12">
                     <div className="contact-card">
                         <i className="contact-icon fas fa-mobile-alt"></i>
-                        <h5>Phone</h5>
+                        <br></br>
                         <a href="tel:16507962557" className="contact">(650) 796-2557</a>
                     </div>
                 </div>
