2ddc302542c3990ddfbf392d566c3f25c5124570
Sarah Vivoli <snvivoli@gmail.com>
2020-04-22 13:55:52 -0700

    build

---
+++ client/build/precache-manifest.563bbdb58f02e1c80580170a2093374f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff7e585b2efea3e5032473af85478ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaf5a12437bd5301a0a1",
+    "url": "/static/css/main.0ead6b0c.chunk.css"
+  },
+  {
+    "revision": "afbcd53045d0b98ef930",
+    "url": "/static/js/2.64c84923.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.64c84923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaf5a12437bd5301a0a1",
+    "url": "/static/js/main.7a7934ad.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "d2116154a53d361efa221bcc06936af0",
+    "url": "/static/media/beach.d2116154.jpg"
+  },
+  {
+    "revision": "a8f3b2d5a5cf231fe7a71e53ab67b58b",
+    "url": "/static/media/books.a8f3b2d5.png"
+  },
+  {
+    "revision": "6d7d9da703464576e4cc042d04c3d570",
+    "url": "/static/media/budget.6d7d9da7.png"
+  },
+  {
+    "revision": "d118a5fa465413a290481f55bc9ee17f",
+    "url": "/static/media/burger.d118a5fa.png"
+  },
+  {
+    "revision": "1cfdb89fe56be5cafa0ecea49d84765c",
+    "url": "/static/media/cahoots.1cfdb89f.png"
+  },
+  {
+    "revision": "f78898ebfb268704438b0bc73fe70919",
+    "url": "/static/media/daily.f78898eb.png"
+  },
+  {
+    "revision": "4b28b0e547774d0107ea0de589be06e8",
+    "url": "/static/media/employee.4b28b0e5.png"
+  },
+  {
+    "revision": "f7589f19391fd108cce8d871eedd01c2",
+    "url": "/static/media/fitness.f7589f19.png"
+  },
+  {
+    "revision": "c9027e426ccb4b43ed5b89684aebce97",
+    "url": "/static/media/note.c9027e42.png"
+  },
+  {
+    "revision": "1371ccddc286c4323d0699b46391cd52",
+    "url": "/static/media/peerup.1371ccdd.jpg"
+  },
+  {
+    "revision": "dcfb1c9a1971d7dc57b1d9184fa33874",
+    "url": "/static/media/portfolio0.dcfb1c9a.png"
+  },
+  {
+    "revision": "5cac9619fcb6b9aded4066ae0ada30e8",
+    "url": "/static/media/portfolio1.5cac9619.png"
+  },
+  {
+    "revision": "6db4537765e5bb0891d11edc44b23c4b",
+    "url": "/static/media/portfolio2.6db45377.png"
+  },
+  {
+    "revision": "7209ff1aae631d399e1eb1ca966c5c66",
+    "url": "/static/media/shows.7209ff1a.png"
+  },
+  {
+    "revision": "8fba348b59fa3f421fb38b256a64e242",
+    "url": "/static/media/weather.8fba348b.png"
+  }
+]);
