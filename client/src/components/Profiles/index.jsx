8c5e6f466656de976d6eaa79322cc9f569bb21ba
Sarah Vivoli <snvivoli@gmail.com>
2020-04-22 12:12:22 -0700

    profiles section grid

--- client/src/components/Profiles/index.jsx
+++ client/src/components/Profiles/index.jsx
@@ -8,18 +8,20 @@ function Profiles() {
                 <h2 className="title">Where to find me</h2>
                 <br></br>
 
-                <div className="uk-grid-collapse uk-child-width-expand@s" uk-grid>
-                    <div className="uk-width-1-3">
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-3 col-4">
                         <a href="https://www.linkedin.com/in/sarah-vivoli/" target="_blank"><i className="social fab fa-linkedin"></i></a>
                     </div>
-                    <div className="profile uk-width-2-3">
+                    <div className="col-lg-8 col-md-8 col-sm-9 col-8">
                         <h4>LinkedIn</h4>
                         <p>Add me on LinkedIn to view my professional network.</p>
                     </div>
-                    <div className="uk-width-1-3">
+                </div>
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-3 col-4">
                         <a href="https://github.com/svivoli" target="_blank"><i className="social fab fa-github"></i></a>
                     </div>
-                    <div className="profile uk-width-2-3">
+                    <div className="col-lg-8 col-md-8 col-sm-9 col-8">
                         <h4>GitHub</h4>
                         <p>Follow me on GitHub to view my projects in depth and get to know me as a
                             developer better.</p>
