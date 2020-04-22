562edfc350777ee2399a7931d95392d3475474d9
Sarah Vivoli <snvivoli@gmail.com>
2020-04-22 12:17:00 -0700

    boot camp section height and margin

--- client/src/components/Program/index.jsx
+++ client/src/components/Program/index.jsx
@@ -4,10 +4,10 @@ import '../style.css';
 function Program() {
     return (
         <section className="bootcamp-section"
-            className="uk-height-large uk-background-cover uk-light uk-flex-vertical-alignment" uk-parallax="bgy: -200" style={{backgroundImage: "url(https://i.imgur.com/E5EeltF.jpg)"}}>
+            className="uk-height-medium uk-background-cover uk-light uk-flex-vertical-alignment" uk-parallax="bgy: -200" style={{backgroundImage: "url(https://i.imgur.com/E5EeltF.jpg)"}}>
 
             <br></br>
-            <h2 className="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical"><a className="bootcamp-link"
+            <h2 className="program-link uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical"><a className="bootcamp-link"
                     href="https://bootcamp.uoregon.edu/coding/" target="_blank">
                     Learn more about the University of Oregon's Boot Camp program here.
                 </a></h2>
