496da4dcac3a4fee4d0ceeb180fa2b4bb62f60a6
Sarah Vivoli <snvivoli@gmail.com>
2020-04-22 15:21:15 -0700

    heroku deploy route fix

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV === "production") {
 // });
 
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
 
 app.listen(PORT, function () {
